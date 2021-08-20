f96dfad69e4b99be4c63136ef621d02190d91282
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-08-20 18:03:21 +0100

    Added event listeners for delete and edit icons

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -23,19 +23,27 @@ const displayTask = (task) => {
       taskHolder.classList.toggle('strikethrough');
     }
 
-    const taskDescription = document.createElement('p');
-    taskDescription.innerText = task.description;
+    const taskDescription = document.createElement('input');
+    taskDescription.classList.add('task-details');
+    // taskDescription.setAttribute('readonly', 'readonly');
+    taskDescription.value = task.description;
 
     const menuButton = document.createElement('span');
-    menuButton.classList.add = 'taskMenu';
+    menuButton.classList.add('taskMenu');
     menuButton.innerHTML = '&#8942;';
+
+    const thrashButton = document.createElement('span');
+    thrashButton.classList.add('trashButton', 'd-none');
+    thrashButton.innerHTML = '&#9986;';
 
     taskHolder.appendChild(taskCheck);
     taskHolder.appendChild(taskDescription);
     taskHolder.appendChild(menuButton);
+    taskHolder.appendChild(thrashButton);
 
     taskListContainer.appendChild(taskHolder);
     addCheckBoxListener(task.index)
+    changeIcon(task.index)
     console.log("added listener for checkbox")
 }
 
@@ -53,6 +61,46 @@ const addCheckBoxListener = (taskId) => {
     changeCompleteStatus(getCurrentList(), targetBox.parentElement.id);
   });
   
+}
+
+const changeIcon = (taskId) => {
+  const taskIcon = document.getElementById(taskId).querySelector(".taskMenu");
+  console.log("I found this" + taskIcon)
+  // inputs = document.querySelectorAll('.text');
+  const trashIcon = document.getElementById(taskId).querySelector(".trashButton");
+
+  taskIcon.previousSibling.addEventListener('focusin', () => {
+    trashIcon.classList.toggle('d-none');
+    taskIcon.classList.toggle('d-none');
+  })
+
+  taskIcon.previousSibling.addEventListener('focusout', () => {
+    trashIcon.classList.toggle('d-none');
+    taskIcon.classList.toggle('d-none');
+  })
+
+  
+  // this.inputs.forEach((input, index) => {
+  //   input.addEventListener('focus', () => {
+  //     this.trashs[index].classList.toggle('d-none');
+  //     input.parentNode.querySelector('.open').classList.toggle('d-none');
+  //     input.parentNode.style.backgroundColor = '#f9f9f9';
+  //     input.style.backgroundColor = '#f9f9f9';
+
+  //     const allLi = document.querySelector('#list').childNodes;
+
+  //     allLi.forEach((list) => {
+  //       const innerInput = list.querySelector('.text');
+
+  //       if (innerInput !== input) {
+  //         innerInput.parentNode.querySelector('.trash').className = 'fa fa-trash-o trash d-none';
+  //         innerInput.parentNode.querySelector('.open').classList.remove('d-none');
+  //         innerInput.parentNode.style.backgroundColor = '';
+  //         innerInput.style.backgroundColor = '';
+  //       }
+  //     });
+  //   });
+  // });
 }
 
 const loadTaskList = () => {
